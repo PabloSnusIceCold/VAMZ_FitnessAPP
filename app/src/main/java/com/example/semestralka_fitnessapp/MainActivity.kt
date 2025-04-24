96e83b4782687aebb45db216f4a669165fbad2b0
PabloSnusIceCold <150700822+PabloSnusIceCold@users.noreply.github.com>
2025-04-24 12:43:21 +0200

    prvy commit

---
+++ app/src/main/java/com/example/semestralka_fitnessapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.semestralka_fitnessapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.semestralka_fitnessapp.ui.theme.Semestralka_FitnessAPPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Semestralka_FitnessAPPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Semestralka_FitnessAPPTheme {
+        Greeting("Android")
+    }
+}
